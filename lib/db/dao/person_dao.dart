fe685bff432002ac3198d8875864251aa91c9a1a
Novjean J Kannathara <novjeanj@gmail.com>
2022-01-15 23:57:17 +0530

    clean up code

--- lib/db/dao/person_dao.dart
+++ lib/db/dao/person_dao.dart
@@ -1,14 +1,13 @@
 import 'package:bloc/db/entity/person.dart';
 import 'package:floor/floor.dart';
-import 'package:flutter/material.dart';
 
 @dao
 abstract class PersonDao {
   @Query('SELECT * FROM Person')
   Future<List<Person>> findAllPersons();
 
-  @Query('SELECT * FROM Person WHERE id IS NOT NULL and id = :id')
-  Stream<Person> findPersonById({@required int id});
+  @Query('SELECT * FROM Person WHERE id = :id')
+  Stream<Person> findPersonById( int id);
 
   @insert
   Future<void> insertPerson(Person person);
--- lib/db/entity/person.dart
+++ lib/db/entity/person.dart
@@ -1,11 +1,10 @@
 
 import 'package:floor/floor.dart';
-import 'package:flutter/material.dart';
 
 @entity
 class Person {
   @primaryKey
   final int id;
-  @required final String name;
-  Person({@required this.id, @required this.name});
+  final String name;
+  Person(this.id, this.name);
 }
