6ccd0c3f6a92e4d3415ff0b9ab0be85ef5065476
Novjean J Kannathara <novjeanj@gmail.com>
2022-01-16 15:43:35 +0530

    resolved owner screen data pull

--- lib/screens/owner_screen.dart
+++ lib/screens/owner_screen.dart
@@ -11,15 +11,17 @@ class OwnerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     logger.i('owner screen is loading...');
-    // final Stream<QuerySnapshot> _citiesStream = FirebaseFirestore.instance.collection('cities').snapshots();
+    final Stream<QuerySnapshot> _citiesStream = FirebaseFirestore.instance.collection('cities').snapshots();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Owner'),
       ),
       drawer: AppDrawer(),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('cities').snapshots(),
+      body:
+
+      StreamBuilder<QuerySnapshot>(
+        stream: _citiesStream,
         builder: (ctx, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
@@ -27,30 +29,53 @@ class OwnerScreen extends StatelessWidget {
             );
           }
 
-          final cityDocs = snapshot.data!.docs.map as Map<String, dynamic>;
-
+          // final document = snapshot.data!.docs.map as DocumentSnapshot;
           // Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
 
-          return GridView.builder(
-            // const keyword can be used so that it does not rebuild when the build method is called
-            // useful for performance improvement
-            padding: const EdgeInsets.all(10.0),
-            itemCount: cityDocs.length,
-            // grid delegate describes how many grids should be there
+          return GridView(
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 1,
               childAspectRatio: 3 / 2,
               crossAxisSpacing: 10,
               mainAxisSpacing: 10,
             ),
-            // item builder defines how the grid should look
-            itemBuilder: (ctx, index) => CityItem(
-              cityDocs[index].id,
-              cityDocs[index].data['name'],
-              cityDocs[index].data()['imageUrl'],
-              key: ValueKey(cityDocs[index].id),
-            ),
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+              return CityItem(
+                document.id,
+                data['name'],
+                data['imageUrl'],
+                key: ValueKey(document.id),
+              );
+              // return ListTile(
+              //   title: Text(data['name']),
+              //   subtitle: Text(data['imageUrl']),
+              // );
+            }).toList(),
           );
+
+          // return GridView(
+          //   // const keyword can be used so that it does not rebuild when the build method is called
+          //   // useful for performance improvement
+          //   padding: const EdgeInsets.all(10.0),
+          //   itemCount: data.length,
+          //   // grid delegate describes how many grids should be there
+          //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          //     crossAxisCount: 1,
+          //     childAspectRatio: 3 / 2,
+          //     crossAxisSpacing: 10,
+          //     mainAxisSpacing: 10,
+          //   ),
+          //   // item builder defines how the grid should look
+          //   itemBuilder: (ctx, index) =>
+          //       // Text(cityDocs[index].id),
+          //   CityItem(
+          //     document[index].id,
+          //     data['name'],
+          //     data['imageUrl'],
+          //     key: ValueKey(document[index].id),
+          //   ),
+          // );
         },
       ),
     );
