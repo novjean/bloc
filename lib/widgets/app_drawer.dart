e96cba2a5619ff49b50d063bed008ce7188f2675
Novjean J Kannathara <novjeanj@gmail.com>
2023-05-03 23:55:29 +0530

    1.5.0

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -149,7 +149,7 @@ class AppDrawer extends StatelessWidget {
             child: Align(
                 alignment: Alignment.bottomRight,
                 child: Text(
-                  'v1.4.8',
+                  'v1.5.0',
                   style: TextStyle(fontSize: 14),
                 )),
           ),
