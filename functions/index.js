2dd49158cbbbebb975aa2eda320c6b680b68906e
Novjean J Kannathara <novjeanj@gmail.com>
2023-09-11 19:30:40 +0530

    2.4.4

--- functions/index.js
+++ functions/index.js
@@ -149,15 +149,20 @@ exports.notificationTestFunction = functions
     .document('notification_tests/{document}')
     .onCreate((snapshot, context) => {
       console.log(snapshot.data());
-      return admin.messaging().sendToTopic('notification_tests', {
-//        notification: {
-//          title: 'notification test',
-//          body: snapshot.data().text,
-//          clickAction: 'FLUTTER_NOTIFICATION_CLICK',
-//        },
+      // Define the message payload (custom data)
+      const message = {
         data: {
           type: 'notification_tests',
-          document: JSON.stringify(snapshot.data()),
+          document: JSON.stringify(snapshot.data())
         },
-      });
+        topic: 'notification_tests',
+      };
+
+      return admin.messaging().send(message)
+        .then((response) => {
+            console.log('notification test message sent successfully:', response);
+        })
+        .catch((error) => {
+            console.error('error sending message:', error);
+        });
     });
