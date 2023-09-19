9c31e344579c7824913d049cb22d3eac6b718989
Novjean J Kannathara <novjeanj@gmail.com>
2023-09-19 15:34:36 +0530

    photos share logic for list view

--- lib/screens/photos/photos_screen.dart
+++ lib/screens/photos/photos_screen.dart
@@ -7,8 +7,13 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:in_app_review/in_app_review.dart';
+import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
+import 'package:multi_select_flutter/util/multi_select_item.dart';
+import 'package:multi_select_flutter/util/multi_select_list_type.dart';
 
 import '../../db/entity/ad.dart';
+import '../../db/entity/lounge.dart';
+import '../../db/entity/lounge_chat.dart';
 import '../../db/entity/user.dart';
 import '../../helpers/dummy.dart';
 import '../../helpers/firestore_helper.dart';
@@ -37,6 +42,9 @@ class _PhotosScreenState extends State<PhotosScreen> {
   List<PartyPhoto> mPartyPhotos = [];
   var _isPartyPhotosLoading = true;
   bool showList = true;
+
+  List<Lounge> sLounges = [];
+  String photoChatMessage = '';
 
   @override
   void initState() {
@@ -261,14 +269,9 @@ class _PhotosScreenState extends State<PhotosScreen> {
               child: TextButton(
                 child: const Text("🪂 share"),
                 onPressed: () {
-                  int fileNum = index + 1;
-                  String fileName = '${partyPhoto.partyName} $fileNum';
-                  String shareText = 'hey. check out this photo and more of ${partyPhoto.partyName} at the official bloc app. Step into the moment. 📸 \n\n🌏 https://bloc.bar/#/\n📱 https://bloc.bar/app_store.html\n\n#blocCommunity ❤️‍🔥';
-
-                  FileUtils.sharePhoto(partyPhoto.id,
-                      partyPhoto.imageUrl, fileName, shareText);
-
                   Navigator.of(ctx).pop();
+                  _showShareOptionsDialog(context, partyPhoto, index);
+
                 },
               ),
             ),
@@ -379,6 +382,264 @@ class _PhotosScreenState extends State<PhotosScreen> {
             ],
           );
         });
+  }
+
+  _showShareOptionsDialog(BuildContext context, PartyPhoto partyPhoto, int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext ctx) {
+        return AlertDialog(
+          title: const Text(
+            'share options',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 22, color: Colors.black),
+          ),
+          backgroundColor: Constants.lightPrimary,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+          contentPadding: const EdgeInsets.all(16.0),
+          content: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.2,
+            width: MediaQuery.of(context).size.width * 0.75,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    width: MediaQuery.of(context).size.width * 0.75,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('share to lounge'),
+                              SizedBox.fromSize(
+                                size: const Size(50, 50),
+                                child: ClipOval(
+                                  child: Material(
+                                    color: Constants.primary,
+                                    child: InkWell(
+                                      splashColor: Constants.darkPrimary,
+                                      onTap: () {
+                                        Navigator.of(ctx).pop();
+                                        _showLoungeChatDialog(context, partyPhoto);
+                                      },
+                                      child: const Column(
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Icon(Icons.share_rounded),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('share to external app'),
+                              SizedBox.fromSize(
+                                size: const Size(50, 50),
+                                child: ClipOval(
+                                  child: Material(
+                                    color: Constants.primary,
+                                    child: InkWell(
+                                      splashColor: Constants.darkPrimary,
+                                      onTap: () async {
+                                        Navigator.of(ctx).pop();
+
+                                        int fileNum = index + 1;
+                                        String fileName = '${partyPhoto.partyName} $fileNum';
+                                        String shareText = 'hey. check out this photo and more of ${partyPhoto.partyName} at the official bloc app. Step into the moment. 📸 \n\n🌏 https://bloc.bar/#/\n📱 https://bloc.bar/app_store.html\n\n#blocCommunity ❤️‍🔥';
+
+                                        FileUtils.sharePhoto(partyPhoto.id,
+                                            partyPhoto.imageUrl, fileName, shareText);
+                                      },
+                                      child: const Column(
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Icon(Icons.share_outlined),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: const Text('close'),
+              onPressed: () {
+                Navigator.of(ctx).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _showLoungeChatDialog(BuildContext context, PartyPhoto partyPhoto) {
+    FirestoreHelper.pullLounges().then((res) {
+      if (res.docs.isNotEmpty) {
+        List<Lounge> lounges = [];
+
+        for (int i = 0; i < res.docs.length; i++) {
+          DocumentSnapshot document = res.docs[i];
+          Map<String, dynamic> map = document.data()! as Map<String, dynamic>;
+          final Lounge lounge = Fresh.freshLoungeMap(map, false);
+
+          if (UserPreferences.getListLounges().contains(lounge.id)) {
+            lounges.add(lounge);
+          }
+        }
+
+        showDialog(
+          context: context,
+          builder: (BuildContext ctx) {
+            return AlertDialog(
+              backgroundColor: Constants.lightPrimary,
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              contentPadding: const EdgeInsets.all(16.0),
+              content: SizedBox(
+                height: mq.height * 0.6,
+                width: mq.width * 0.9,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+                        child: Text(
+                          'share photo to lounge',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(height:12),
+                      MultiSelectDialogField(
+                        items: lounges
+                            .map((e) => MultiSelectItem(e, e.name))
+                            .toList(),
+                        initialValue: sLounges.map((e) => e).toList(),
+                        listType: MultiSelectListType.CHIP,
+                        buttonIcon: Icon(
+                          Icons.arrow_drop_down,
+                          color: Colors.grey.shade700,
+                        ),
+                        title: const Text('select lounges to share'),
+                        buttonText: const Text(
+                          'select lounge *',
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius:
+                          const BorderRadius.all(Radius.circular(5)),
+                          border: Border.all(
+                            width: 0.0,
+                          ),
+                        ),
+                        searchable: true,
+                        onConfirm: (values) {
+                          sLounges = values as List<Lounge>;
+                        },
+                      ),
+                      const SizedBox(height:12),
+                      Center(
+                          child: SizedBox(
+                            width: mq.width,
+                            child: FadeInImage(
+                              placeholder:
+                              const AssetImage('assets/images/logo_3x2.png'),
+                              image: NetworkImage(
+                                  partyPhoto.imageThumbUrl.isNotEmpty
+                                      ? partyPhoto.imageThumbUrl
+                                      : partyPhoto.imageUrl),
+                              fit: BoxFit.contain,
+                            ),
+                          )),
+                      TextFieldWidget(
+                        text: '',
+                        maxLines: 3,
+                        onChanged: (text) {
+                          photoChatMessage = text;
+                        },
+                        label: 'message',
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              actions: [
+                TextButton(
+                  child: const Text("cancel"),
+                  onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Constants
+                        .darkPrimary),
+                  ),
+                  child: const Text(
+                    "💌 send",
+                    style: TextStyle(color: Constants.primary),
+                  ),
+                  onPressed: () {
+                    if (sLounges.isNotEmpty) {
+                      String message =
+                          '${partyPhoto.imageUrl},$photoChatMessage';
+
+                      for (Lounge lounge in sLounges) {
+                        LoungeChat chat = Dummy.getDummyLoungeChat();
+                        chat = chat.copyWith(
+                          message: message,
+                          type: 'image',
+                          loungeId: lounge.id,
+                          loungeName: lounge.name,
+                        );
+
+                        FirestoreHelper.pushLoungeChat(chat);
+                        FirestoreHelper.updateLoungeLastChat(lounge.id, '📸 $photoChatMessage', chat.time);
+                      }
+
+                      Logx.ist(_TAG, 'photo has been shared 💝');
+                      Navigator.of(ctx).pop();
+                    } else {
+                      Logx.ist(_TAG,
+                          '🙃 select at least one lounge to share this photo to');
+                    }
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        Logx.est(_TAG, '🫤 something went wrong, please try again!');
+      }
+    });
   }
 
   _showDownloadAppDialog(BuildContext context) {
