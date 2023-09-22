09ed27f719905505292f1bf5ce4dbc52db3e465b
Novjean J Kannathara <novjeanj@gmail.com>
2023-09-22 12:39:27 +0530

    turning the crashlytics on for flutter error

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ Future<void> main() async {
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
   PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: false);
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
     return true;
   };
 
