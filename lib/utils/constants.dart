8b2555a883ac48f1b6d6f599a94e9fe2d547e316
Novjean J Kannathara <novjeanj@gmail.com>
2023-09-23 16:33:06 +0530

    2.6.4

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const String kAppTitle = 'bloc';
 
 class Constants {
-  static const String appVersion = '2.6.3';
+  static const String appVersion = '2.6.4';
 
   static const String fontDefault = 'Oswald';
 
