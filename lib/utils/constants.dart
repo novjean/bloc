303ed3d809b651824f898b331a5e63e170965336
Novjean J Kannathara <novjeanj@gmail.com>
2024-01-20 20:22:41 +0530

    4.5.3

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,7 +5,7 @@ const String kAppTitle = 'bloc.';
 class Constants {
 
   //todo: check all testMode before release
-  static const String appVersion = '4.5.2';
+  static const String appVersion = '4.5.3';
 
   static const String fontDefault = 'Oswald';
 
