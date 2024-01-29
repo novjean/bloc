ad305cd0b3b02f3b3f1e383bb5f15ffbe802f442
Novjean J Kannathara <novjeanj@gmail.com>
2024-01-29 13:16:28 +0530

    added web keywords

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'dart:ui';
 
 import 'package:bloc/db/shared_preferences/party_guest_preferences.dart';
-import 'package:bloc/db/shared_preferences/table_preferences.dart';
 import 'package:bloc/routes/bloc_router.dart';
 
 import 'package:bloc/db/shared_preferences/ui_preferences.dart';
-import 'package:bloc/screens/ui/splash_screen.dart';
 import 'package:bloc/services/firebase_api.dart';
 import 'package:bloc/utils/logx.dart';
-import 'package:bloc/widgets/ui/loading_widget.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
@@ -17,11 +14,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:logger/logger.dart';
-import 'package:provider/provider.dart';
 
 import 'db/shared_preferences/user_preferences.dart';
 import 'firebase_options.dart';
-import 'providers/cart.dart';
 import 'utils/constants.dart';
 
 var logger = Logger(
@@ -44,45 +39,10 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await FirebaseAppCheck.instance.activate(
-    // You can also use a `ReCaptchaEnterpriseProvider` provider instance as an
-    // argument for `webProvider`
     webProvider: ReCaptchaV3Provider('6Lc_XlYpAAAAAMf1fEA7WkNQBleWV48l1vXRb0Dx'),
-    // Default provider for Android is the Play Integrity provider. You can use the "AndroidProvider" enum to choose
-    // your preferred provider. Choose from:
-    // 1. Debug provider
-    // 2. Safety Net provider
-    // 3. Play Integrity provider
     androidProvider: AndroidProvider.playIntegrity,
-    // Default provider for iOS/macOS is the Device Check provider. You can use the "AppleProvider" enum to choose
-    // your preferred provider. Choose from:
-    // 1. Debug provider
-    // 2. Device Check provider
-    // 3. App Attest provider
-    // 4. App Attest provider with fallback to Device Check provider (App Attest provider is only available on iOS 14.0+, macOS 14.0+)
     appleProvider: AppleProvider.appAttest,
   );
-
-  // Listen for Auth changes and .refresh the GoRouter [router]
-  // FirebaseAuth.instance.authStateChanges().listen((User? user) {
-  //
-  //   FirestoreHelper.pullUser(user!.uid).then((res) {
-  //     if(res.docs.isNotEmpty){
-  //       DocumentSnapshot document = res.docs[0];
-  //       Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-  //
-  //       final blocUser.User mUser = Fresh.freshUserMap(data, false);
-  //       // UserPreferences.setUser(mUser);
-  //       //
-  //       Logx.i(_TAG, 'main: auth state change. user ${mUser.name}');
-  //       //
-  //       // BlocRouter.returnRouter(true).refresh();
-  //     } else {
-  //       Logx.em(_TAG, 'user not found');
-  //     }
-  //   });
-  // });
-
-
 
   if (!kIsWeb) {
     await FirebaseApi().initNotifications();
@@ -186,154 +146,5 @@ class _BlocAppState extends State<BlocApp> {
       ),
       routerConfig: BlocRouter.returnRouter(true),
     );
-
-    // final Future<FirebaseApp> initFirebase = Firebase.initializeApp();
-
-    // return FutureBuilder(
-    //   // Initialize FlutterFire:
-    //     future: initFirebase,
-    //     builder: (ctx, appSnapshot) {
-    //       switch (appSnapshot.connectionState) {
-    //         case ConnectionState.waiting:
-    //         case ConnectionState.none:
-    //           return SplashScreen();
-    //       // return const LoadingWidget();
-    //         case ConnectionState.active:
-    //         case ConnectionState.done:
-    //           {
-    //             Logx.i(_TAG, 'firebase initialized');
-    //
-    //             return MaterialApp.router(
-    //               debugShowCheckedModeBanner: false,
-    //               title: kAppTitle,
-    //               theme: ThemeData(
-    //                 primaryColor: Constants.primary,
-    //                 primaryColorLight: Constants.lightPrimary,
-    //                 primaryColorDark: Constants.darkPrimary,
-    //
-    //                 backgroundColor: Constants.background,
-    //                 shadowColor: const Color.fromRGBO(158, 158, 158, 1.0),
-    //
-    //                 highlightColor: const Color.fromRGBO(255, 255, 255, 1.0),
-    //                 bottomAppBarColor:
-    //                 const Color.fromRGBO(255, 255, 255, 1.0),
-    //
-    //                 // app bar and buttons by default
-    //                 primarySwatch: Colors.brown,
-    //                 fontFamily: Constants.fontDefault,
-    //
-    //                 buttonTheme: ButtonTheme.of(context).copyWith(
-    //                   buttonColor: Colors.red,
-    //                   textTheme: ButtonTextTheme.primary,
-    //                   shape: RoundedRectangleBorder(
-    //                     borderRadius: BorderRadius.circular(20),
-    //                   ),
-    //                 ),
-    //
-    //                 textTheme: const TextTheme(
-    //                   bodyLarge: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                   bodyMedium: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                   bodySmall: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                   labelLarge: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                   labelMedium: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                   labelSmall: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                   // You can customize other text styles as well
-    //                 ),
-    //
-    //                 textButtonTheme: TextButtonThemeData(
-    //                   style: TextButton.styleFrom(
-    //                     foregroundColor: Constants.darkPrimary,
-    //                   ),
-    //                 ),
-    //
-    //                 elevatedButtonTheme: ElevatedButtonThemeData(
-    //                   style: ElevatedButton.styleFrom(
-    //                       backgroundColor: Constants.lightPrimary,
-    //                       foregroundColor: Constants.darkPrimary
-    //                   ),
-    //                 ),
-    //               ),
-    //               routerConfig: BlocRouter.returnRouter(true),
-    //             );
-    //           }
-    //       }
-    //     });
-
-    // return MultiProvider(
-    //   providers: [
-    //     ChangeNotifierProvider(
-    //       create: (ctx) => Cart(),
-    //     ),
-    //   ],
-    //   child: FutureBuilder(
-    //       // Initialize FlutterFire:
-    //       future: initFirebase,
-    //       builder: (ctx, appSnapshot) {
-    //         switch (appSnapshot.connectionState) {
-    //           case ConnectionState.waiting:
-    //           case ConnectionState.none:
-    //             return SplashScreen();
-    //             // return const LoadingWidget();
-    //           case ConnectionState.active:
-    //           case ConnectionState.done:
-    //             {
-    //               Logx.i(_TAG, 'firebase initialized');
-    //
-    //               return MaterialApp.router(
-    //                 debugShowCheckedModeBanner: false,
-    //                 title: kAppTitle,
-    //                 theme: ThemeData(
-    //                   primaryColor: Constants.primary,
-    //                   primaryColorLight: Constants.lightPrimary,
-    //                   primaryColorDark: Constants.darkPrimary,
-    //
-    //                   backgroundColor: Constants.background,
-    //                   shadowColor: const Color.fromRGBO(158, 158, 158, 1.0),
-    //
-    //                   highlightColor: const Color.fromRGBO(255, 255, 255, 1.0),
-    //                   bottomAppBarColor:
-    //                       const Color.fromRGBO(255, 255, 255, 1.0),
-    //
-    //                   // app bar and buttons by default
-    //                   primarySwatch: Colors.brown,
-    //                   fontFamily: Constants.fontDefault,
-    //
-    //                   buttonTheme: ButtonTheme.of(context).copyWith(
-    //                     buttonColor: Colors.red,
-    //                     textTheme: ButtonTextTheme.primary,
-    //                     shape: RoundedRectangleBorder(
-    //                       borderRadius: BorderRadius.circular(20),
-    //                     ),
-    //                   ),
-    //
-    //                   textTheme: const TextTheme(
-    //                     bodyLarge: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                     bodyMedium: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                     bodySmall: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                     labelLarge: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                     labelMedium: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                     labelSmall: TextStyle(color: Constants.darkPrimary, fontFamily: Constants.fontDefault),
-    //                     // You can customize other text styles as well
-    //                   ),
-    //
-    //                   textButtonTheme: TextButtonThemeData(
-    //                     style: TextButton.styleFrom(
-    //                       foregroundColor: Constants.darkPrimary,
-    //                     ),
-    //                   ),
-    //
-    //                   elevatedButtonTheme: ElevatedButtonThemeData(
-    //                     style: ElevatedButton.styleFrom(
-    //                       backgroundColor: Constants.lightPrimary,
-    //                       foregroundColor: Constants.darkPrimary
-    //                     ),
-    //                   ),
-    //                 ),
-    //                 routerConfig: BlocRouter.returnRouter(true),
-    //               );
-    //             }
-    //         }
-    //       }),
-    // );
   }
 }
